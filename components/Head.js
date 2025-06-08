26241c55bb254ff25c7c774c0d524e56486afc86
moicben <benjamain.georges@gmail.com>
2025-06-08 22:42:42 +0700

    new pixel

--- components/Head.js
+++ components/Head.js
@@ -113,7 +113,7 @@ const MyHead = ({ title, description, name, domain, favicon, graph, font, colorP
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
+              t.src=v;s=b.getElementsByTagName(e)[0]; 
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '${pixel}');
@@ -124,7 +124,7 @@ const MyHead = ({ title, description, name, domain, favicon, graph, font, colorP
             <img height="1" width="1" style={{display:'none'}}
             src={`https://www.facebook.com/tr?id=${pixel}&ev=PageView&noscript=1`}
             />
-          </noscript>
+          </noscript> 
         </>
       )}
 
